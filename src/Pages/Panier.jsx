492d6dbd0978c381f5ae5be655166c31172756a1
ozkansoyturk <soyturkozkan@gmail.com>
2022-01-21 09:46:54 +0100

    salut

---
+++ src/Pages/Panier.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { Fragment, useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, SelectorIcon } from "@heroicons/react/solid";
+import panier from "../panier.png";
+import { Switch } from "@headlessui/react";
+
+const people = [
+  { name: "hebdomadaire" },
+  { name: "mensuel" },
+  { name: "trimestriel" },
+  { name: "annuel" },
+];
+
+const Panier = () => {
+  const [selected, setSelected] = useState(people[0]);
+  const [enabled, setEnabled] = useState(false);
+
+  return (
+    <div>
+      <img src={panier} className="panier" alt="panier" />
+      <p id="commande" className="text-lg">
+        Commande récurrente ?
+      </p>
+
+      <Switch
+        checked={enabled}
+        onChange={setEnabled}
+        className={`${enabled ? "bg-yellow-500" : "bg-stone-500"}
+          switch relative inline-flex flex-shrink-0 h-[24px] w-[45px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+      >
+        <span className="sr-only">Use setting</span>
+        <span
+          aria-hidden="true"
+          className={`${enabled ? "translate-x-5" : "translate-x-0"}
+            pointer-events-none inline-block h-[20px] w-[20px] rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200`}
+        />
+      </Switch>
+
+      <div className="listbox w-60 text-center">
+        <Listbox value={selected} onChange={setSelected}>
+          <div className="relative mt-1">
+            <Listbox.Button className="relative w-60 py-2 pl-3 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
+              <span className="block truncate text-center	">
+                {selected.name}
+              </span>
+              <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                <SelectorIcon
+                  className="w-5 h-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </span>
+            </Listbox.Button>
+            <Transition
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute w-60  mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                {people.map((person, personIdx) => (
+                  <Listbox.Option
+                    key={personIdx}
+                    className={({ active }) =>
+                      `${
+                        active ? "text-amber-900 bg-amber-100" : "text-gray-900"
+                      }
+                          cursor-default select-none relative py-2 pl-10 pr-4`
+                    }
+                    value={person}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`${
+                            selected ? "font-medium" : "font-normal"
+                          } block truncate`}
+                        >
+                          {person.name}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`${
+                              active ? "text-amber-600" : "text-amber-600"
+                            }
+                                absolute inset-y-0 left-0 flex items-center pl-3`}
+                          >
+                            <CheckIcon className="w-5 h-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        </Listbox>
+      </div>
+    </div>
+  );
+};
+
+export default Panier;
